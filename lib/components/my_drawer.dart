d5767189bfa3e2934713d575fe16d3866e02870b
Animesh Bhattarai <animesh.bhattarai2019@gmail.com>
2024-06-14 23:47:30 +0545

    worked more on drawer behaviour and cart page

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,119 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:quick_cart/pages/cart_page.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      child: Column(
-        children: [
-          // Drawer header (logo) positioned a bit down
-          Container(
-            padding: const EdgeInsets.only(top: 40.0, bottom: 20.0),
-            color: Theme.of(context).colorScheme.background,
-            child: Center(
-              child: Icon(
-                Icons.shopping_bag,
-                size: 100,
-                color: Theme.of(context).colorScheme.inversePrimary,
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        Navigator.of(context).pop(); // Close the drawer when tapping outside the list items
+      },
+      child: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        child: Column(
+          children: [
+            // Drawer header (logo) positioned a bit down
+            Container(
+              padding: const EdgeInsets.only(top: 40.0, bottom: 20.0),
+              color: Theme.of(context).colorScheme.background,
+              child: Center(
+                child: Icon(
+                  Icons.shopping_bag,
+                  size: 80,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
               ),
             ),
-          ),
-          // Spacer to push the following items to the middle
-          Spacer(flex: 1),
-          // Shop tile
-          Center(
-            child: ListTile(
-              title: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.shop,
+            // Spacer to push the following items to the middle
+            Spacer(flex: 1),
+            // Shop tile (Home)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  size: 30,
+                ),
+                title: Text(
+                  'Shop',
+                  style: TextStyle(
                     color: Theme.of(context).colorScheme.inversePrimary,
-                    size: 30, // Increased icon size
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
-                  const SizedBox(width: 16), // Spacer between icon and text
-                  Text(
-                    'Shop',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20, // Increased text size
-                    ),
-                  ),
-                ],
+                ),
+                onTap: () {
+                  Navigator.of(context).pop(); // Close the drawer
+                },
               ),
-              onTap: () {
-                // Navigate to shop page
-              },
             ),
-          ),
-          const SizedBox(height: 20), // Spacer between Shop and Cart tiles
-          // Cart tile
-          Center(
-            child: ListTile(
-              title: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.shopping_cart,
+            const SizedBox(height: 20), // Spacer between Home and Cart tiles
+            // Cart tile
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.shopping_cart,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  size: 30,
+                ),
+                title: Text(
+                  'Cart',
+                  style: TextStyle(
                     color: Theme.of(context).colorScheme.inversePrimary,
-                    size: 30, // Increased icon size
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
-                  const SizedBox(width: 16), // Spacer between icon and text
-                  Text(
-                    'Cart',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20, // Increased text size
-                    ),
-                  ),
-                ],
+                ),
+                onTap: () {
+                  Navigator.of(context).pop(); // Close the drawer
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CartPage()),
+                  );
+                },
               ),
-              onTap: () {
-                // Navigate to cart page
-              },
             ),
-          ),
-          // Spacer to push the exit tile upward
-          Spacer(flex: 5),
-          // Exit shop tile
-          Center(
-            child: ListTile(
-              title: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.exit_to_app,
+            // Spacer to push the exit tile upward
+            Spacer(flex: 5),
+            // Exit shop tile
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.exit_to_app,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  size: 30,
+                ),
+                title: Text(
+                  'Exit',
+                  style: TextStyle(
                     color: Theme.of(context).colorScheme.inversePrimary,
-                    size: 30, // Increased icon size
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
-                  const SizedBox(width: 20), // Spacer between icon and text
-                  Text(
-                    'Exit',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20, // Increased text size
-                    ),
-                  ),
-                ],
+                ),
+                onTap: () {
+                  Navigator.of(context).pop(); // Close the drawer
+                },
               ),
-              onTap: () {
-                // Exit the shop
-              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
