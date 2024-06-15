e783b64099869d7b3354a99b0840b09b74021aaa
Animesh Bhattarai <animesh.bhattarai2019@gmail.com>
2024-06-15 23:46:29 +0545

    started shop modeling

---
+++ lib/models/shop.dart
@@ -0,0 +1,30 @@
+import 'package:quick_cart/models/product.dart';
+
+class Shop {
+
+  // products for sale
+  final List<Product> _shop = [
+
+    // product 1
+    Product(
+      name: "Product 1",
+      price: 100,
+      description: "Item description...", 
+      imagepath: "hahaha",
+    ),
+  ];
+
+  // user cart
+
+
+  // get product list
+
+
+  // get user cart
+
+
+  // add items to cart
+
+
+  // remove the cart items
+}
